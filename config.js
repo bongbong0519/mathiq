6915e34fcffe8d5fc7a8b815d7f1d7dadfd76104
bongbong0519 <yoonbro1927@gmail.com>
2026-04-09 03:44:46 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = 'https://cmwuuksxziyemknwwvkc';
+const SUPABASE_URL = 'https://cmwuuksxziyemknwwvkc.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_e1Xu-klaPd_bZKrKsZ8YsA_WEBnRWRh';
